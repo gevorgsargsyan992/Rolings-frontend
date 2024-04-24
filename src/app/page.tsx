4329fc3dff17084fb1e16c68d16c04f7bea6dce0
anahit1212 <alaverdyan.anahit@gmail.com>
2024-04-24 22:27:16 +0400

    uncomment the homepage code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 'use client'
+import Image from 'next/image';
+import construction from '../../public/construction.svg'
+import logo from '../../public/logo.svg'
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center bg-gray-100">
-      {/* <Image src={logo} width={260} alt="Under Construction"></Image> */}
-      {/* <Image src={construction} width={790} alt="Under Construction" /> */}
+      <Image src={logo} width={200} alt="Under Construction"></Image>
+      <Image src={construction} width={500} alt="Under Construction" />
       <p className=" text-xl text-gray-700 text-center">
         We currently working on this page. Please check back later!
       </p>
