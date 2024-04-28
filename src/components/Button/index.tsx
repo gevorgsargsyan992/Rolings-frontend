94d5016ae1cc965b4f3aeb1ebdb19efd89892f17
anahit1212 <alaverdyan.anahit@gmail.com>
2024-04-28 12:59:58 +0400

    format the code and create constants

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,39 +1,44 @@
 "use client";
-import React, {useMemo} from "react";
+import React, { useMemo } from "react";
 import { ButtonProps } from "./types";
-import Typography from "@/components/Typography";
-
-const {Text} = Typography
+import {SMALL, MEDIUM, LARGE, PRIMARY,SECONDARY, GHOST} from './consts'
 
 const Button: React.FC<ButtonProps> = ({
-  type = "primary",
-  size = "medium",
+  type = PRIMARY,
+  size = MEDIUM,
   iconLeft,
   iconRight,
   children,
   onClick,
-                                         className,
-    ...props
+  className,
+  ...props
 }) => {
   const buttonSizeClass = useMemo(() => {
     switch (size) {
-      case 'small': return "px-3 py-1 text-sm";
-      case 'medium': return "px-4 py-2 text-base"
-      case 'large': return  "px-4 py-2 text-lg"
-      default: return "px-4 py-2 text-lg"
+      case SMALL:
+        return "px-3 py-1 text-sm";
+      case MEDIUM:
+        return "px-4 py-2 text-base";
+      case LARGE:
+        return "px-4 py-2 text-lg";
+      default:
+        return "px-4 py-2 text-lg";
     }
-  },[size])
+  }, [size]);
 
   const buttonTypeClass = useMemo(() => {
-    switch(type){
-      case 'ghost': return "border bg-transparent border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white";
-      case 'secondary': return "bg-gray-500 text-white focus:ring-gray-500";
-      case 'primary': return "bg-blue-500 hover:bg-blue-600 text-white focus:ring-blue-500";
+    switch (type) {
+      case GHOST:
+        return "border bg-transparent border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white";
+      case SECONDARY:
+        return "bg-gray-500 text-white focus:ring-gray-500";
+      case PRIMARY:
+        return "bg-blue-500 hover:bg-blue-600 text-white focus:ring-blue-500";
 
-      default: return "bg-blue-500 hover:bg-blue-600 text-white focus:ring-blue-500";
+      default:
+        return "bg-blue-500 hover:bg-blue-600 text-white focus:ring-blue-500";
     }
-  },[type])
-
+  }, [type]);
 
   return (
     <button
@@ -42,7 +47,7 @@ const Button: React.FC<ButtonProps> = ({
       {...props}
     >
       {iconLeft && <span className="mr-2">{iconLeft}</span>}
-     {children}
+      {children}
       {iconRight && <span className="ml-2">{iconRight}</span>}
     </button>
   );
--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -1,11 +1,11 @@
-import {MouseEvent, ReactNode} from "react";
+import { MouseEvent, ReactNode } from "react";
 
 export interface ButtonProps {
-    type?: 'primary' | 'secondary' | 'ghost';
-    size?: 'small' | 'medium' | 'large';
-    iconLeft?: ReactNode;
-    iconRight?: ReactNode;
-    children?: ReactNode;
-    onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
-    className?: string;
+  type?: string;
+  size?: string;
+  iconLeft?: ReactNode;
+  iconRight?: ReactNode;
+  children?: ReactNode;
+  onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
+  className?: string;
 }
