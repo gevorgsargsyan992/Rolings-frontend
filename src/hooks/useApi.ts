dda1889c51fc391be7c543fbba078e1cd978446d
anahit1212 <alaverdyan.anahit@gmail.com>
2024-05-05 23:01:31 +0400

    comment the file

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,46 +1,46 @@
-import { useState } from 'react';
-import axios, { AxiosResponse, AxiosError, AxiosRequestConfig } from 'axios';
+// import { useState } from 'react';
+// import axios, { AxiosResponse, AxiosError, AxiosRequestConfig } from 'axios';
 
-interface ApiResponse<T> {
-  loading: boolean;
-  error: AxiosError | null;
-  get: (url: string) => Promise<T>;
-  post: (url: string, data: any) => Promise<T>;
-}
+// interface ApiResponse<T> {
+//   loading: boolean;
+//   error: AxiosError | null;
+//   get: (url: string) => Promise<T>;
+//   post: (url: string, data: any) => Promise<T>;
+// }
 
-const useApi = <T>() : ApiResponse<T> => {
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<AxiosError | null>(null);
+// const useApi = <T>() : ApiResponse<T> => {
+//   const [loading, setLoading] = useState<boolean>(false);
+//   const [error, setError] = useState<AxiosError | null>(null);
 
-  const makeRequest = async (method: AxiosRequestConfig['method'], url: string, data: any = null) => {
-    setLoading(true);
-    setError(null);
+//   const makeRequest = async (method: AxiosRequestConfig['method'], url: string, data: any = null) => {
+//     setLoading(true);
+//     setError(null);
 
-    try {
-      const response: AxiosResponse<T> = await axios.request({
-        method,
-        url,
-        data,
-      });
-      return response.data;
-    } catch (error) {
-      setError(error as AxiosError);
-      throw error; 
-    } finally {
-      setLoading(false);
-    }
-  };
+//     try {
+//       const response: AxiosResponse<T> = await axios.request({
+//         method,
+//         url,
+//         data,
+//       });
+//       return response.data;
+//     } catch (error) {
+//       setError(error as AxiosError);
+//       throw error; 
+//     } finally {
+//       setLoading(false);
+//     }
+//   };
 
-  const get = async (url: string) => {
-    return makeRequest('GET', url);
-  };
+//   const get = async (url: string) => {
+//     return makeRequest('GET', url);
+//   };
 
-  const post = async (url: string, data: any) => {
-    return makeRequest('POST', url, data);
-  };
+//   const post = async (url: string, data: any) => {
+//     return makeRequest('POST', url, data);
+//   };
 
 
-  return { loading, error, get, post };
-};
+//   return { loading, error, get, post };
+// };
 
-export default useApi;
+// export default useApi;
