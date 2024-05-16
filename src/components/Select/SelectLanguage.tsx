0eae3a1848697db0e36348d23f9d1714fc42d39e
gevorg.sargsyan <gevorg.sargsyan@softconstruct.com>
2024-05-16 14:46:33 +0400

    Fixed redirection after click on language option

--- src/components/Select/SelectLanguage.tsx
+++ src/components/Select/SelectLanguage.tsx
@@ -1,46 +1,19 @@
-'use client';
+"use client";
 
-import { useRouter } from 'next/navigation';
-import { usePathname } from 'next/navigation';
-import { useTranslation } from 'react-i18next';
-import i18nConfig from '../../../i18nConfig'
+import Link from "next/link";
+import i18nConfig from "../../../i18nConfig";
+import { localeNameMap } from "../../constants/locales";
 
 export default function LanguageChanger() {
-    const { i18n } = useTranslation();
-    const currentLocale = i18n.language;
-    const router = useRouter();
-    const currentPathname = usePathname();
-
-    const handleChange = e => {
-        const newLocale = e.target.value;
-
-        // set cookie for next-i18n-router
-        const days = 30;
-        const date = new Date();
-        date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-        const expires = date.toUTCString();
-        document.cookie = `NEXT_LOCALE=${newLocale};expires=${expires};path=/`;
-
-        // redirect to the new locale path
-        if (
-            currentLocale === i18nConfig.defaultLocale &&
-            !i18nConfig.prefixDefault
-        ) {
-            router.push('/' + newLocale + currentPathname);
-        } else {
-            router.push(
-                currentPathname.replace(`/${currentLocale}`, `/${newLocale}`)
-            );
-        }
-
-        router.refresh();
-    };
-
-    return (
-        <select onChange={handleChange} value={currentLocale}>
-            <option value="en">Eng</option>
-            <option value="hy">Arm</option>
-            <option value="ru">Rus</option>
-        </select>
-    );
+  return (
+    <ul className="flex xs:text-sm lg:text-lg bg-red lg:mt-0 xs:mt-4 content-center p-2 ">
+      {i18nConfig.locales.map((localeCode) => (
+        <li key={localeCode} className="text-gray-900 border-8 ">
+          <Link scroll={false} href={`/${localeCode}`}>
+            {localeNameMap[localeCode]}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 }
