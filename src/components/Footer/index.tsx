4826313b307823e28215a2663586f33bc58505cf
AnahitAlaverdyan <anahit.alaverdyan@softconstruct.com>
2024-05-17 18:59:09 +0400

    footer changes

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,16 @@
+"use client";
 import Image from "next/image";
 import logo from "../../../public/rolings-logo.svg";
+import Typography from "@/components/Typography";
+import Link from "next/link";
+import Input from "@/components/Input";
+import { DATA } from "./contsnts";
+import { useState } from "react";
+const { Text } = Typography;
 
 const Footer = () => {
+  const [email, setEmail] = useState<string>("");
+
   return (
     <>
       <div className="max-w-5xl w-full py-8 bg-blue-grayish">
@@ -15,60 +24,62 @@ const Footer = () => {
       </div>
 
       <footer className="w-full bg-white p-8">
-        <div className="max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-5 gap-6">
-          <div className="col-span-1 md:col-span-4 grid grid-cols-2 md:grid-cols-3 gap-2">
-            <div>
-              <h5 className="font-bold">Company</h5>
-              <ul>
-                <li>
-                  <a href="/about">About Us</a>
-                </li>
-                <li>
-                  <a href="/contact">Contact</a>
-                </li>
-                <li>
-                  <a href="/careers">Careers</a>
-                </li>
-              </ul>
+        <div className="max-w-5xl mx-auto gap-6">
+          <div className="flex w-full justify-between gap-2">
+            <div className="flex flex-col gap-y-4">
+              <div>
+                <svg
+                  className="w-6 h-6 text-gray-500"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 18 16"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"
+                  />
+                </svg>
+                <Text level={5}>Armenia / YerevanAbovyan 20</Text>
+              </div>
+
+              <Text level={5}>info@rolings.am</Text>
+              <Text level={5}>+374 44 410 002</Text>
             </div>
+            {DATA.map((elem, idx) => (
+              <div className="flex flex-col" key={idx}>
+                {elem?.title && (
+                  <Text className="font-bold mb-4">{elem?.title}</Text>
+                )}
+                <div className="flex flex-col gap-y-1">
+                  {elem?.data.map((data, idx) => (
+                    <Link
+                      key={idx}
+                      className="align-baseline text-gray-dim"
+                      href={`${data?.link}`}
+                      passHref
+                    >
+                      {data?.text}
+                    </Link>
+                  ))}
+                </div>
+              </div>
+            ))}
             <div>
-              <h5 className="font-bold">Resources</h5>
-              <ul>
-                <li>
-                  <a href="/blog">Blog</a>
-                </li>
-                <li>
-                  <a href="/news">News</a>
-                </li>
-                <li>
-                  <a href="/support">Support</a>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h5 className="font-bold">Legal</h5>
-              <ul>
-                <li>
-                  <a href="/privacy">Privacy Policy</a>
-                </li>
-                <li>
-                  <a href="/terms">Terms of Use</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="col-span-1 md:col-span-1 flex flex-col justify-between">
-            <div>
-              <h5 className="font-bold mb-2">Join Our Newsletter</h5>
+              <Text className="font-bold mb-4">Join Our Newsletter</Text>
               <form className="flex flex-col space-y-4">
-                <input
+                <Input
+                  value={email}
+                  onChange={setEmail}
                   type="email"
                   placeholder="Enter your email"
-                  className="px-4 py-2 rounded-md text-gray-800"
+                  className="px-4 py-2 rounded-md text-gray-800 border border-gray-5"
                 />
                 <button
                   type="submit"
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md"
+                  className="bg-black hover:bg-gray-900 text-white font-bold py-2 px-4 rounded-md"
                 >
                   Subscribe
                 </button>
