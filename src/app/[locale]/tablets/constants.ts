8fc2b042abd7d0ea0350578bf72b4f24eecf1000
gevorg.sargsyan <gevorg.sargsyan@softconstruct.com>
2024-06-01 11:23:19 +0400

    FIxed colors

--- src/app/[locale]/tablets/constants.ts
+++ src/app/[locale]/tablets/constants.ts
@@ -1,15 +1,16 @@
-import {Column} from './types'
+import { Column } from "./types";
 
 export const TabletStatus = {
-    1: 'Active',
-    2: 'NOT ACTIVE',
-    3: 'LIVE VIDEO',
-    4: 'STOPPED VIDEO',
-}
+  1: "Active",
+  2: "NOT ACTIVE",
+  3: "LIVE VIDEO",
+  4: "STOPPED VIDEO",
+};
 
-export const COLUMNS: Column[]  = [
-    { key: "createdAt", label: "Time" },
-    { key: "tabletStatus", label: "Status" },
-    { key: "tb_uuid", label: "UUID" },
-    { key: "videoCount", label: "Video Count" }
+export const COLUMNS: Column[] = [
+  { key: "id", label: "ID" },
+  { key: "tabletStatus", label: "Status" },
+  { key: "tb_uuid", label: "UUID" },
+  { key: "videoCount", label: "Video Count" },
+  { key: "createdAt", label: "Time" },
 ];
