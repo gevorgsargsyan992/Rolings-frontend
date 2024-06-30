e4e4ce3e74e0db7305f76e4ead721c1c7637fe84
anahit1212 <alaverdyan.anahit@gmail.com>
2024-06-30 23:43:41 +0400

    add route permisiions

--- src/types/UserTypes.ts
+++ src/types/UserTypes.ts
@@ -1,6 +1,15 @@
 export enum UserType {
-    SUPER_ADMIN = 1,
-    ADMIN = 2,
-    MANAGER = 3,
-    SELLER = 4,
+  SUPER_ADMIN = 1,
+  ADMIN = 2,
+  MANAGER = 3,
+  SELLER = 4,
 }
+
+export interface Permissions {
+  [key: string]: UserType[];
+}
+
+export const permissions: Permissions = {
+  viewDashboard: [UserType.SUPER_ADMIN, UserType.ADMIN, UserType.MANAGER, UserType.SELLER],
+  viewSettings: [UserType.SUPER_ADMIN],
+};
