30c67bd3fdd5956ddbd01ba2a3d076316455bba0
AnahitAlaverdyan <anahit.alaverdyan@softconstruct.com>
2024-09-16 17:32:35 +0400

    some design changes

--- src/app/[locale]/homepage/components/Hero/index.tsx
+++ src/app/[locale]/homepage/components/Hero/index.tsx
@@ -3,6 +3,7 @@ import { FC } from "react";
 import { useTranslation } from "react-i18next";
 import Typography from "@/components/Typography";
 import Advantages from "./components/Advantages";
+import PageContainer from "@/components/PageContainer";
 
 const { Text } = Typography;
 
@@ -11,61 +12,70 @@ const Hero: FC = () => {
   const currentLanguage = window.localStorage.getItem("language");
 
   return (
-    <div className="flex flex-col w-full">
-      <div className="flex flex-col md:flex-row w-full">
-        <div className="flex flex-1 flex-col md:mr-4">
-          <div className="flex justify-center md:justify-start">
-            <div className="bg-blue-royal rounded-full text-white px-4 py-2 text-base mr-0 md:mr-4 text-xs md:text-sm text-center">
-              {t("stay-focused")}
+    <>
+      <PageContainer>
+        <div className="flex flex-col w-full">
+          <div className="flex flex-col md:flex-row w-full">
+            <div className="flex flex-1 flex-col md:mr-4">
+              <div className="flex justify-center md:justify-start">
+                <div className="bg-blue-royal rounded-full text-white px-4 py-2 text-base mr-0 md:mr-4 text-xs md:text-sm text-center">
+                  {t("stay-focused")}
+                </div>
+              </div>
+              <div className="flex flex-col h-full justify-between">
+                <div className="flex flex-col pt-10 text-center md:text-start">
+                  <Text
+                    className="xl:text-4xl lg:text-2xl text-xl"
+                    color="text-gray-900"
+                  >
+                    {t("first-interactive")}
+                  </Text>
+                  <Text
+                    className="xl:text-4xl lg:text-2xl text-xl"
+                    color="text-gray-900"
+                  >
+                    {t("advertisement")}
+                  </Text>
+                  <Text
+                    className="uppercase mt-4 xl:text-4xl lg:text-2xl text-xl"
+                    color="text-blue-500"
+                  >
+                    {t("in-armenia")}
+                  </Text>
+                </div>
+                <Text
+                  bold
+                  color="text-black"
+                  className="mt-2 text-center md:text-start md:text-base text-sm"
+                >
+                  {t("advatages-our-network")}
+                </Text>
+              </div>
+            </div>
+            <div className="flex flex-1 w-full mt-8 md:mt-0 md:block">
+              <div className="relative w-full h-full">
+                <video
+                  className="inset-0 w-full h-full object-cover rounded-xl"
+                  src={
+                    currentLanguage
+                      ? `/rolings${currentLanguage}.mov`
+                      : `/rolingsen.mov`
+                  }
+                  autoPlay
+                  loop
+                  muted
+                  playsInline
+                />
+              </div>
             </div>
           </div>
-          <div className="flex flex-col h-full justify-between">
-            <div className="flex flex-col pt-10 text-center md:text-start">
-              <Text
-                className="xl:text-4xl lg:text-2xl text-xl"
-                color="text-gray-900"
-              >
-                {t("first-interactive")}
-              </Text>
-              <Text
-                className="xl:text-4xl lg:text-2xl text-xl"
-                color="text-gray-900"
-              >
-                {t("advertisement")}
-              </Text>
-              <Text
-                className="uppercase mt-4 xl:text-4xl lg:text-2xl text-xl"
-                color="text-blue-500"
-              >
-                {t('in-armenia')}
-              </Text>
-            </div>
-            <Text
-              bold
-              color="text-black"
-              className="mt-2 text-center md:text-start md:text-base text-sm"
-            >
-              {t("advatages-our-network")}
-            </Text>
+          <div className="flex flex-col w-full pt-10">
+            <Advantages />
           </div>
         </div>
-        <div className="flex flex-1 w-full mt-8 md:mt-0 md:block">
-          <div className="relative w-full h-full">
-            <video
-              className="inset-0 w-full h-full object-cover rounded-xl"
-              src={currentLanguage ? `/rolings${currentLanguage}.mov` : `/rolingsen.mov`}
-              autoPlay
-              loop
-              muted
-              playsInline
-            />
-          </div>
-        </div>
-      </div>
-      <div className="flex flex-col w-full pt-10">
-        <Advantages />
-      </div>
-    </div>
+      </PageContainer>
+      <div className="bg-white h-14 w-full mt-6" />
+    </>
   );
 };
 
--- src/app/[locale]/homepage/components/InfoSection/index.tsx
+++ src/app/[locale]/homepage/components/InfoSection/index.tsx
@@ -4,34 +4,38 @@ import Typography from "@/components/Typography";
 import ideaImg from "../../../../../../public/idea.webp";
 // import Button from "@/components/Button";
 import { useTranslation } from "react-i18next";
+import PageContainer from "@/components/PageContainer";
 
 const { Text } = Typography;
 
 const InfoSection = () => {
   const { t } = useTranslation() as any;
   return (
-    <div className="flex items-center lg:items-start flex-col lg:flex-row gap-4 py-8">
-      <div className="flex lg:w-full max-w-[500px] lg:max-w-[704px] lg:flex-1">
-        <Image src={ideaImg} alt="idea" className="rounded-xl" />
-      </div>
-      <div className="flex flex-1 items-center lg:items-start flex-col">
-        {/*<div className="bg-blue-royal max-w-max rounded-full text-white px-4 py-2 text-base mr-0 text-xs md:text-sm">*/}
-        {/*  {t("start-driving-message")}*/}
-        {/*</div>*/}
-        {/*<Text*/}
-        {/*  className="uppercase xl:mt-4 mt-3 lg:text-2xl text-xl"*/}
-        {/*  color="text-blue-500"*/}
-        {/*>*/}
-        {/*  {t("elevate-game")}*/}
-        {/*</Text>*/}
-        <Text className="xl:text-base text-sm lg:mt-0 mt-4 lg:text-left text-center text-black">
-          {t("looking-to-reach-audience")}
-        </Text>
-        {/*<Button type="ghost" className="max-w-max xl:mt-6 mt-4">*/}
-        {/*  Go to the section*/}
-        {/*</Button>*/}
-      </div>
-    </div>
+    <>
+      <PageContainer className="flex items-center lg:items-start flex-col lg:flex-row gap-4 py-8">
+        <div className="flex lg:w-full max-w-[500px] lg:max-w-[704px] lg:flex-1 mr-0 lg:mr-6">
+          <Image src={ideaImg} alt="idea" className="rounded-xl" />
+        </div>
+        <div className="flex flex-1 items-center lg:items-start flex-col pt-2">
+          {/*<div className="bg-blue-royal max-w-max rounded-full text-white px-4 py-2 text-base mr-0 text-xs md:text-sm">*/}
+          {/*  {t("start-driving-message")}*/}
+          {/*</div>*/}
+          {/*<Text*/}
+          {/*  className="uppercase xl:mt-4 mt-3 lg:text-2xl text-xl"*/}
+          {/*  color="text-blue-500"*/}
+          {/*>*/}
+          {/*  {t("elevate-game")}*/}
+          {/*</Text>*/}
+          <Text className="xl:text-base text-sm lg:mt-0 mt-4 lg:text-left text-center self-center text-black">
+            {t("looking-to-reach-audience")}
+          </Text>
+          {/*<Button type="ghost" className="max-w-max xl:mt-6 mt-4">*/}
+          {/*  Go to the section*/}
+          {/*</Button>*/}
+        </div>
+      </PageContainer>
+      <div className="bg-white h-14 w-full mt-2" />
+    </>
   );
 };
 
