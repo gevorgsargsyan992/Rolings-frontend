5f364117ee580f394b1dcffd79aaafc3f786bf58
AnahitAlaverdyan <anahit.alaverdyan@softconstruct.com>
2024-12-19 15:03:14 +0400

    set loading for save button

--- src/app/[locale]/vehicle/[id]/page.tsx
+++ src/app/[locale]/vehicle/[id]/page.tsx
@@ -80,24 +80,27 @@ const VehicleDetail: FC = () => {
   };
 
   const handleSaveChanges = async () => {
-    setLoadingSave(true);
     try {
+      setLoadingSave(true);
       const { licensePlate, name, status, color } = vehicle;
-      await put(`${VEHICLE}/${id}`, {
-        tabletId: +selectedTablet,
-        action: "UPDATE",
-      });
+      if (originalTablet != selectedTablet) {
+        await put(`${VEHICLE}/${id}`, {
+          tabletId: +selectedTablet,
+          action: "UPDATE",
+        });
+      }
       await patch(`${VEHICLE}/${id}`, {
         licensePlate,
         name,
-        status,
+        status: +status,
         color,
       });
       setIsEditing(false);
+      setLoadingSave(false);
     } catch (err) {
+      setLoadingSave(false);
       console.error("Error saving changes:", err);
     }
-    setLoadingSave(false);
   };
 
   return (
