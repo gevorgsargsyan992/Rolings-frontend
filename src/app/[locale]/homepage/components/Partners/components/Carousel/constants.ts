fca395288787de3402e7664e3c931386124b4bb3
AnahitAlaverdyan <anahit.alaverdyan@softconstruct.com>
2025-07-02 18:53:09 +0400

    add new partners and removed unused ones

--- src/app/[locale]/homepage/components/Partners/components/Carousel/constants.ts
+++ src/app/[locale]/homepage/components/Partners/components/Carousel/constants.ts
@@ -1,17 +1,19 @@
 import loyal from "../../../../../../../../public/loyal.webp";
 import ameria from "../../../../../../../../public/ameria.webp";
-import exterior from "../../../../../../../../public/exterior.webp";
 import autolab from "../../../../../../../../public/autolab.webp";
 import skill from "../../../../../../../../public/skill.webp";
-import blaze from "../../../../../../../../public/blaze.webp";
+import bestLife from "../../../../../../../../public/best-life.webp";
+import noa from "../../../../../../../../public/noa.webp";
+import samsung from "../../../../../../../../public/samsung.webp";
 import publicis from "../../../../../../../../public/publicis.webp";
 
 export const DATA = [
   { image: ameria, url: "https://ameriabank.am/", height: 36 },
   { image: autolab, url: "https://www.autolab.am/", height: 14 },
-  { image: exterior, url: "https://exterior.am/", height: 40 },
   { image: loyal, url: "https://loyal.am/hy", height: 60 },
-  { image: blaze, url: "", height: 60 },
+  { image: bestLife, url: "https://www.bestlife.am/", height: 70 },
+  { image: noa, url: "https://noahsharbor.com/", height: 75 },
+  { image: samsung, url: "https://www.samsung.com/ru/armenia/", height: 80 },
   { image: publicis, url: "https://publicis.am/", height: 60 },
   { image: skill, url: "https://skill.am/", height: 50 },
 ];
