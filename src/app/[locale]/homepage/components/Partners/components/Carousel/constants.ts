7ae849241ee0819cc4ebbc6f30f7c46b67a068d5
AnahitAlaverdyan <anahit.alaverdyan@softconstruct.com>
2025-07-03 18:29:08 +0400

    add new partners and removed unused ones

--- src/app/[locale]/homepage/components/Partners/components/Carousel/constants.ts
+++ src/app/[locale]/homepage/components/Partners/components/Carousel/constants.ts
@@ -3,7 +3,7 @@ import ameria from "../../../../../../../../public/ameria.webp";
 import autolab from "../../../../../../../../public/autolab.webp";
 import skill from "../../../../../../../../public/skill.webp";
 import bestLife from "../../../../../../../../public/best-life.webp";
-import noa from "../../../../../../../../public/noah.webp";
+import noa from "../../../../../../../../public/noah-new.webp";
 import samsung from "../../../../../../../../public/samsung.webp";
 import publicis from "../../../../../../../../public/publicis.webp";
 
@@ -13,7 +13,7 @@ export const DATA = [
   { image: bestLife, url: "https://www.bestlife.am/", height: 70 },
   { image: autolab, url: "https://www.autolab.am/", height: 14 },
   { image: loyal, url: "https://loyal.am/hy", height: 60 },
-  { image: noa, url: "https://noahsharbor.com/", height: 90 },
+  { image: noa, url: "https://noahsharbor.com/", height: 60 },
   { image: publicis, url: "https://publicis.am/", height: 60 },
   { image: skill, url: "https://skill.am/", height: 50 },
 ];
