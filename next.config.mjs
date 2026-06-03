dfa882f3062d62927d0d7442ada7515dc5061a55
gevorg.sargsyan <gevs.sargsyan92@gmail.com>
2026-06-03 17:47:28 +0400

    Added unoptimized

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
   env: {
     PUBLIC_APP_AUTH_URL: process.env.APP_AUTH_URL,
   },
