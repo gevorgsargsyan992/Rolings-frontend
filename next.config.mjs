315c55ef9731b70415d28499fd7b7b4d24306a6f
Gevorg Sargsyan <77688151+gevorgsargsyan992@users.noreply.github.com>
2026-06-03 18:03:01 +0400

    Remove .next (#175)

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
   env: {
     PUBLIC_APP_AUTH_URL: process.env.APP_AUTH_URL,
   },
